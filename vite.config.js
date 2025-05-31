6f536ce1819cd436acaf887028223bf3d667fa1d
brettenf <brettenfarrell33@gmail.com>
2025-05-30 23:04:34 -0700

    feat: Add gh-pages deployment script and update Vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Cert_Mint/', // Replace with your actual repo name
 })
